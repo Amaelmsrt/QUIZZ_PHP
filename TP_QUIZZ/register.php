48bb083bd3235cad8bede948798dec2dfb784743
Maserati Amael <o22202871@info23-18.iut45.univ-orleans.fr>
2024-01-22 16:05:33 +0100

    on peut s'enregistrer

--- TP_QUIZZ/register.php
+++ TP_QUIZZ/register.php
@@ -1,33 +1,37 @@
 <?php
 
-$file_db=new PDO('sqlite:users.sqlite3');
+$file_db = new PDO('sqlite:users.sqlite3');
+$error_message = '';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
     $passwordVerify = $_POST['passwordVerify'];
+
     if ($password != $passwordVerify) {
         $error_message = "Passwords don't match";
-    }
-    // Vérifier si l'utilisateur existe déjà
-    $checkIfExists = $file_db->prepare("SELECT COUNT(*) FROM users WHERE username = :username");
-    $checkIfExists->bindParam(':username', $username);
-    $checkIfExists->execute();
-    if ($checkIfExists->fetchColumn() > 0) {
-        echo "<p style='color: red;'>Ce nmo existe déjà</p>";
     } else {
-        $stmt = $file_db->prepare('INSERT INTO users (username, password) VALUES (:username, :password)');
-        $stmt->bindParam(':username', $username);
-        $stmt->bindParam(':password', $password);
-        $stmt->execute();
-        header('Location: login.php');
-        exit;
+        // Vérifier si l'utilisateur existe déjà
+        $checkIfExists = $file_db->prepare("SELECT COUNT(*) FROM users WHERE username = :username");
+        $checkIfExists->bindParam(':username', $username);
+        $checkIfExists->execute();
+
+        if ($checkIfExists->fetchColumn() > 0) {
+            $error_message = "Ce nom existe déjà";
+        } else {
+            $stmt = $file_db->prepare('INSERT INTO users (username, password) VALUES (:username, :password)');
+            $stmt->bindParam(':username', $username);
+            $stmt->bindParam(':password', $password);
+            $stmt->execute();
+            echo '<script>window.location.href = "login.php";</script>';
+            exit;
+        }
     }
-    
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-
 
 <head>
     <meta charset="UTF-8">
@@ -40,7 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <h1>Register</h1>
 
     <?php
-    if (isset($error_message)) {
+    if (!empty($error_message)) {
         echo "<p style='color: red;'>$error_message</p>";
     }
     ?>
@@ -57,10 +61,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </div>
 
             <div id="buttons">
-                <a href="register.php">Register</a>
+                <input type="submit" value="Register">
             </div>
-            
         </form>
     </div>
 </body>
+
 </html>
